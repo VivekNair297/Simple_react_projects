3a99788d268a1d36cd7057a1eda0185eade7e0b4
Vivek Nair <viveknair297@gmail.com>
2022-03-03 01:16:16 +1100

    Adding a simple react project from net ninja youtube channel.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './home';
+
+function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
