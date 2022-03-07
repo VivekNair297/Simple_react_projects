9dda860d24a18b37067588312c543ffa32aca211
Vivek Nair <viveknair297@gmail.com>
2022-03-07 22:19:34 +1100

    Learning how to use useEffect and how to add useEffect dependencies in React.

--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import Bloglist from "./BlogList";
 
 const Home = () => {
@@ -7,11 +8,24 @@ const Home = () => {
         {title :'Welcome party!', body : 'lorem ipsum ...', author : 'luigi', id : 2},
         {title :'Web dev top tips', body : 'lorem ipsum ...', author : 'mario', id : 3}
     ]);
+
+    const [name, setName] = useState('mario');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('use effect run');
+        console.log(name);
+    }, [name]);
     
     return (
         <div className="home">
-            <Bloglist blogs={blogs} title="All Blogs!" />
-            <Bloglist blogs={blogs.filter((blog) => blog.author ==='mario')} title="Mario's blogs" />
+            <Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('Luigi')}>Change name</button>
+            <p>{ name }</p>
         </div>
       );
 }
